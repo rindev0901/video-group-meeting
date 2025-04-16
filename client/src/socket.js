b7b194c88f6583c248eea5caebe33005782d3442
Nguyen Huynh Duc <nguyenduc09012003@gmail.com>
2025-04-16 12:18:37 +0700

    update

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,9 @@
 import io from "socket.io-client";
 
-const ENDPOINT = "http://localhost:3001";
+const ENDPOINT =
+  process.env.NODE_ENV === "production"
+    ? window.location.origin
+    : "http://localhost:3001";
 
 const socket = io(ENDPOINT, {
   autoConnect: true,
