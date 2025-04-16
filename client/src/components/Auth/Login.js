ee9d5eb4ff461843e36b00262be70b1abf3fb6da
Nguyen Huynh Duc <nguyenduc09012003@gmail.com>
2025-04-16 12:32:03 +0700

    update

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -62,8 +62,8 @@ export default function Login() {
       margin: "0 auto",
       padding: screens.md
         ? `${token.paddingXL}px`
-        : `${token.sizeXXL}px ${token.padding}px`,
-      width: "380px",
+        : `${token.sizeXXL}px ${token.sizeXXL}px`,
+        width: '300px'
     },
     footer: {
       marginTop: token.marginLG,
--- client/src/components/Auth/Register.js
+++ client/src/components/Auth/Register.js
@@ -61,8 +61,8 @@ export default function Register() {
       margin: "0 auto",
       padding: screens.md
         ? `${token.paddingXL}px`
-        : `${token.paddingXL}px ${token.padding}px`,
-      width: "380px",
+        : `${token.sizeXXL}px ${token.sizeXXL}px`,
+      width: '300px'
     },
     forgotPassword: {
       float: "right",
